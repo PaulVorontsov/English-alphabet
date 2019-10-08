2aa47e0f7a830625913c73616e3a7e0eb51783d3
Your Name <pavelvorontsov666@gmail.com>
2019-10-08 18:53:45 +0300

    added content, styles, audio files, js files

---
+++ block/js/script1.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('common1'); //
+
+button.addEventListener('click', myPlay);
+
+function myPlay() {
+    var a = document.getElementById('Apple');
+    a.play();
+} ;
---
+++ block/js/script2.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('common2'); //
+
+button.addEventListener('click', myPlay);
+
+function myPlay() {
+    var a = document.getElementById('Acorn');
+    a.play();
+} ;
